4e78a920ac0b14810baea9b57dc09a437232934e
devikann <devikann25@gmail.com>
2026-05-24 12:41:06 +0530

    fixed app.jsx routing error

--- src/context/App.jsx
+++ src/context/App.jsx
@@ -3,27 +3,30 @@ import Login from './Pages/Login';
 import Home from './Pages/Home';
 import Menu from './Pages/Menu';
 import Cart from './Pages/Cart';
-import AdminDashboard from './Pages/admin/Dashboard'; // We will do this next
-import { CartProvider } from './context/CartContext';
-// Add this import
+import AdminDashboard from './Pages/admin/Dashboard';
 import ManageMenu from './Pages/admin/ManageMenu';
-
-// Add this route inside <Routes>
-<Route path="/admin/manage" element={<ManageMenu />} />
+import { CartProvider } from './context/CartContext';
 
 function App() {
   return (
     <CartProvider>
       <Router>
         <Routes>
+          {/* Public Route */}
           <Route path="/" element={<Login />} />
+
+          {/* Student Routes */}
           <Route path="/home" element={<Home />} />
           <Route path="/menu/:categoryName" element={<Menu />} />
           <Route path="/cart" element={<Cart />} />
+
+          {/* Admin Routes */}
           <Route path="/admin" element={<AdminDashboard />} />
+          <Route path="/admin/manage" element={<ManageMenu />} />
         </Routes>
       </Router>
     </CartProvider>
   );
 }
+
 export default App;
